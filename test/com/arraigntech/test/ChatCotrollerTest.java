099b2feee6297a5678fe3df5fddfc4d20ceccc59
flashbhaskar <bhaskar@arraigntech.com>
2021-10-20 22:16:19 +0530

    chnages to chat bot

---
+++ test/com/arraigntech/test/ChatCotrollerTest.java
@@ -0,0 +1,13 @@
+package com.arraigntech.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author Bhaskara S
+ *
+ */
+//@RunWith(JUnit4.class)  
+public class ChatCotrollerTest {
+
+}
